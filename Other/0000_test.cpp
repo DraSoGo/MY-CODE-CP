a78c403652ac0cec00b70fd43b7dbc380d2284b9
DraSoGo <vdhfmfatv4321@gmail.com>
2024-04-01 16:51:53 +0700

    01/04/2024

--- Other/0000_test.cpp
+++ Other/0000_test.cpp
@@ -1,10 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+int tree[1000];
+
+int sum(int k)
+{
+    int s = 0;
+    while (k >= 1)
+    {
+        s += tree[k];
+        k -= k&-k;
+    }
+    return s;
+}
+
+void add(int k,int x)
+{
+    while (k <= n)
+    {
+        tree[k] += x;
+        k += k&-k;
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);cin.tie();cout.tie(0);
-    cout << "hi";
+    int x,a,b;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> x;
+        add(i,x);
+    }
+    cin >> a >> b;
+    cout << sum(b)-sum(a); 
     return 0;
 }
