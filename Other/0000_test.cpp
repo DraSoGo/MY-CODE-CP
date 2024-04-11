ec5a4ddc08461d8c8427cb2b202539dbc4f31e9e
DraSoGo <vdhfmfatv4321@gmail.com>
2024-04-11 22:43:09 +0700

    11/04/2024

--- Other/0000_test.cpp
+++ Other/0000_test.cpp
@@ -1,48 +1,77 @@
 #include <bits/stdc++.h>
-#define f first
-#define s second
-#define ll long long
+#define coutf(n, m) cout << fixed << setprecision(n) << m
+#define forr(i, a, n) for (int i = a; i < n; i++)
+#define forl(i, a, n) for (int i = a; i > n; i--)
+#define macos                \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define endll "\n"
+#define sp " "
+typedef long long ll;
 using namespace std;
+
+struct Non
+{
+    int u, v, w;
+
+    bool operator<(const Non &rhs) const
+    {
+        if (w != rhs.w)
+            return w < rhs.w;
+        return v > rhs.v;
+    }
+};
+
+int par[3001];
+int findpar(int x)
+{
+    if (par[x] != x)
+        par[x] = findpar(par[x]);
+    return par[x];
+}
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m;
-    cin >> n >> m;
-    int room[m];
-    int num;
-    int m_num = 0;
-    for (int i = 0; i < m; ++i)
+    macos;
+
+    int n, e, a, b, c, d, k;
+    cin >> n >> e;
+    forr(i, 0, n) par[i] = i;
+    vector<Non> edges;
+    forr(i, 0, e)
     {
-        cin >> room[i];
-    }
-    int l = 0, r = 1e9, mid;
-    while (l < r)
-    {
-        mid = (l + r) / 2;
-        cout << l << " " << r << " " << mid << "\n";
-        int c = 1, sum = 0;
-        for (int i = 0; i < m; ++i)
-        {
-            if (room[i] > mid)
-            {
-                c += n;
-                break;
-            }
-            sum += room[i];
-            if (sum > mid)
-            {
-                c++;
-                sum = room[i];
-            }
-            // cout << sum << " " << c << "\n";
-        }
-        if (c > n)
-            l = mid + 1;
+        cin >> a >> b >> c >> d;
+        if (!d)
+            edges.push_back({a, b, c});
         else
-            r = mid;
+            par[findpar(b)] = par[findpar(a)];
     }
-    cout << r;
+    sort(edges.begin(), edges.end());
+
+    cin >> k;
+    vector<pair<int, int>> package;
+    forr(i, 0, k)
+    {
+        cin >> a >> b;
+        package.push_back({a, b});
+    }
+    sort(package.begin(), package.end());
+
+    int ans = 0;
+    forr(i, 0, e)
+    {
+        auto [u, v, w] = edges[i];
+        if (findpar(u) == findpar(v))
+            continue;
+        int mn = 1e9;
+        for (auto p : package)
+            if (p.first >= w)
+                mn = min(mn, p.second);
+        ans += mn;
+        par[findpar(u)] = par[findpar(v)];
+    }
+    cout << ans;
+
     return 0;
 }
---
+++ jspwTest.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Graph
+{
+    int v,w,st;
+    bool operator < (const Graph & a)const
+    {
+        return w < a.w;
+    }
+};
+
+vector <Graph> V[10001];
+priority_queue <Graph> PQ[10001];
+int dist[10001][10001];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n,a,b,e,u,v,w;
+    cin >> n >> a >> b >> e;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> u >> v >> w;
+        V[u].push_back({v,w});
+    }
+    // memset(dist,99999,sizeof(dist));
+    for (int i = 0; i < 10001; i++)
+    {
+        for (int j = 0; j < 10001; j++)
+        {
+            dist[i][j] = INT_MAX;
+        }
+    }
+    dist[a][0] = 0;
+    PQ.push({a,0,0});
+
+    return 0;
+}
