cbaad701fad6a6c140819ead17dfa31d3f931faa
DraSoGo <vdhfmfatv4321@gmail.com>
2024-04-16 12:42:25 +0700

    15/04/2024

--- Other/0000_test.cpp
+++ Other/0000_test.cpp
@@ -1,67 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-
-int dist[2200][2200];
-
-struct A
-{
-    int v, w, cnt;
-    bool operator<(const A &o) const
-    {
-        return w > o.w;
-    }
-};
-
-vector<pair<int, int>> adj[2200];
-priority_queue<A> pq;
-
+int DP[500001];
+deque<pair<int, int>> dq;
 int main()
 {
-    cin.tie(0)->sync_with_stdio(0);
-    int n, p, u, e;
-    int l;
-    cin >> n >> p >> u >> e;
-    for (int i = 1, q, r, t; i <= e; i++)
+    int n, k, val, i;
+    cin >> n >> k;
+    for (i = 1; i <= n; i++)
     {
-        cin >> q >> r >> t;
-        adj[q].push_back({r, t});
-    }
-    memset(dist, 0x3f, sizeof dist);
-    pq.push({p, dist[p][0] = 0});
-    while (!pq.empty())
-    {
-        A now = pq.top();
-        pq.pop();
-        if (now.cnt >= n)
-            continue;
-        // cout << now.v << " " << now.w << " " << now.cnt << "\n";
-        int o = 0;
-        cout << now.v << "\n";
-        for (auto x : adj[now.v])
+        cin >> val;
+        for(auto j:dq)
         {
-            cout << o << "\n";
-            int nxt = now.w + x.second; // sum weight
-            if (dist[x.first][now.cnt + 1] > nxt)
-            {
-                // cout << nxt << "\n";
-                pq.push({x.first, dist[x.first][now.cnt + 1] = nxt, now.cnt + 1}); // if new weight is less than current
-            }
-            o++;
+            cout << j.second << " :" << j.first << "\n";
         }
-    }
-    cin >> l;
-    while (l--)
-    {
-        int y;
-        cin >> y;
-        int mn = 2e9;
-        for (int i = 1; i <= n; i++)
+        while (!dq.empty() && dq.front().second + k < i)
         {
-            // cout << dist[u][i] << "\n";
-            mn = min(mn, dist[u][i] + (i * y) - y);
+            // cout << dq.front().second + k << "\n";
+            dq.pop_front();
         }
-        // cout << mn << "\n\n";
+        if (!dq.empty())
+        {
+            val += dq.front().first;
+        }
+        DP[i] = val;
+        cout << DP[i] << "\n";
+        while (!dq.empty() && dq.back().first > val)
+        {
+            dq.pop_back();
+        }
+        dq.push_back({DP[i], i});
+        for(auto j:dq)
+        {
+            cout << j.second << " :" << j.first << "\n";
+        }
+        cout << "---\n";
     }
-    return 0;
+    cout << DP[n];
 }
--- TOI/TOI08_electricity.cpp
+++ TOI/TOI08_electricity.cpp
@@ -2,42 +2,29 @@
 using namespace std;
 int main()
 {
-    int n, k, sum = 0, ch;
-    deque<int> Q;
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n, k,x;
     cin >> n >> k;
-    int A[n];
+    int DP[n],A[n];
+    deque <int> idx;
     for (int i = 0; i < n; i++)
     {
         cin >> A[i];
+        while (!idx.empty() && idx.front()+k < i)
+        {
+            idx.pop_front();
+        }
+        if (!idx.empty())
+        {
+             A[i] += DP[idx.front()];
+        }
+        DP[i] = A[i];
+        while (!idx.empty() && DP[i] < DP[idx.back()])
+        {
+            idx.pop_back();
+        }
+        idx.push_back(i);
     }
-    sum += A[0];
-    //cout << 0 << " ";
-    Q.push_back(1);
-    for (int i = 2; i < n; i++)
-    {
-        if (i == n-1)
-        {
-            sum += A[i];
-            break;
-        }
-        if (A[i] > A[Q.front()])
-        {
-            Q.push_back(i);
-        }
-        if (A[i] <= A[Q.front()])
-        {
-            //cout << i << "," << A[i] << " " << Q.front() << "," << A[Q.front()] << "\n";
-            Q.clear();
-            Q.push_back(i);
-            //cout << A[Q.front()] << "\n";
-            sum += A[Q.front()];
-        }
-        if (i - Q.front() == k)
-        {
-            Q.pop_front();
-            sum += A[Q.front()];
-            //cout << Q.front() << " ";
-        }
-    }
-    cout << sum;
+    cout << DP[n-1];
+    return 0;
 }
