20f476270b640088328d71ee69e0ad5c7697becf
DraSoGo <vdhfmfatv4321@gmail.com>
2024-04-19 13:04:46 +0700

    18/04/2024

--- Other/0000_test.cpp
+++ Other/0000_test.cpp
@@ -1,47 +1,59 @@
 #include <bits/stdc++.h>
-#define coutf(n, m) cout << fixed << setprecision(n) << m
-#define forr(i, a, n) for (int i = a; i < n; i++)
-#define forl(i, a, n) for (int i = a; i > n; i--)
-#define macos                \
-    ios::sync_with_stdio(0); \
-    cin.tie(0);              \
-    cout.tie(0)
-#define endll "\n"
-#define sp " "
-typedef long long ll;
 using namespace std;
-const int N = 1010;
-ll A[N];
+using ll = long long int;
+const int N = 505;
+ll x[N], y[N], dp[N][N];
+
 int main()
 {
-    macos;
-
-    ll n, k;
-    cin >> n >> k;
-    forr(i, 0, n) cin >> A[i];
-    sort(A, A + n);
-    ll l = 0, r = 1e9, m, all;
-    while (l < r)
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
     {
-        m = (l + r) >> 1;
-        all = n * (m + 1) * (m + 1);
-        forr(i, 1, n)
-        {
-            ll range = A[i] - A[i - 1] - 1;
-            ll plant_grow = m - (range/2);
-            if (plant_grow <= 0)
-                continue;
-            if (range & 1)
-                all -= plant_grow * plant_grow;
-            else
-                all -= plant_grow * (plant_grow + 1);
-        }
-        if (all < k)
-            l = m + 1;
-        else
-            r = m;
+        cin >> x[i] >> y[i];
+        dp[1][i] = abs(x[i] - x[1]) + abs(y[i] - y[1]);
     }
-    cout << l;
-
-    return 0;
+    for (int k = 2; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            dp[k][i] = 1e18;
+            for (int j = 1; j <= n; j++)
+            {
+                dp[k][i] = min(dp[k][i], max(dp[k - 1][j], abs(x[i] - x[j]) + abs(y[i] - y[j])));
+            }
+        }
+    }
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            cout << dp[k][i] << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    for (int k = 1; k <= n; k++)
+    {
+        cout << dp[k][n] << " ";
+    }
+    cout << "\n";
+    ll ans = 0;
+    while (m--)
+    {
+        ll x;
+        cin >> x;
+        for (int i = 1; i <= n; i++)
+        {
+            if (x >= dp[i][n])
+            {
+                ans += i;
+                break;
+            }
+        }
+        //		cout << ans << ' ' <<x << "\n";
+    }
+    cout << ans;
 }
--- TOI/TOI17_metaverse1.cpp
+++ TOI/TOI17_metaverse1.cpp
@@ -3,25 +3,99 @@ using namespace std;
 
 struct Graph
 {
-    int v,w,t,ct;
+    int u,w,t,ct;
+    bool operator < (const Graph & b)const
+    {
+        return w > b.w;
+    }
 };
 
 vector <Graph> adj[40003];
+priority_queue <Graph> PQ;
+int dist[40003][100];
+int vis[40003][100];
+pair <int,int> xy[40003];
+int m,n,k,p;
 
 int dis(int x1,int y1,int x2,int y2)
 {
     return abs(x1-x2)+abs(y1-y2);
 }
 
+void DJ(int st,int ed)
+{
+    dist[st][0] = 0;
+    // vis[st][0] = 1;
+    PQ.push({st,0,0,0});
+    while (!PQ.empty())
+    {
+        auto [u,w,t,ct] = PQ.top();
+        cout << u << " " << w << " " << ct << "\n";
+        PQ.pop();
+        if (ct >= p)
+        {
+            continue;
+        }
+        for (int i = 0; i < adj[u].size(); i++)
+        {
+            if (dist[adj[u][i].u][ct + adj[u][i].t] > adj[u][i].w + w)
+            {
+                dist[adj[u][i].u][ct + adj[u][i].t] = adj[u][i].w + w;
+                PQ.push({adj[u][i].u,dist[adj[u][i].u][ct + adj[u][i].t],t,ct + adj[u][i].t});
+            }
+        }
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int m,n,k,p,a = 0,b = 1,x1,y1,x2,y2;
+    int a = 0,b = 1,x1,y1,x2,y2;
     cin >> m >> n >> k >> p;
+    for (int i = 0; i < 40003; i++)
+    {
+        for (int j = 0; j < 100; j++)
+        {
+            dist[i][j] = INT_MAX; 
+            vis[i][j] = 0;
+        }
+    }
     adj[a].push_back({b,dis(0,0,m,n),0,0});
-    for (int i = 0; i < k; i++)
+    for (int i = 2; i < (2*k)+2; i+=2)
     {
         cin >> x1 >> y1 >> x2 >> y2;
+        xy[i] = {x1,y1};
+        xy[i+1] = {x2,y2};
+        adj[a].push_back({i,dis(0,0,x1,y1),0,0});
+        adj[i].push_back({i+1,0,1,0});
+        adj[i+1].push_back({b,dis(x2,y2,m,n),0,0});
     }
+    for (int i = 3; i < (2*k)+2; i+=2)
+    {
+        for (int j = 1; j < adj[a].size(); j++)
+        {
+            if (adj[a][j].u != i-1)
+            {
+                adj[i].push_back({adj[a][j].u,dis(xy[i].first,xy[i].second,xy[adj[a][j].u].first,xy[adj[a][j].u].second),0,0});
+            }
+        }
+    }
+    for (int i = 0; i < 2*k+2; i++)
+    {
+        cout << i << " :";
+        for (int j = 0; j < adj[i].size(); j++)
+        {
+            cout << adj[i][j].u << "," << adj[i][j].w << " ";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    DJ(0,1);
+    cout << "\n";
+    for (int i = 0; i <= p; i++)
+    {
+        cout << dist[2][i] << "\n";
+    }
+    
     return 0;
 }
