ce323bd4a7e3bc98de438f8fddbc6b471ead0a7f
DraSoGo <vdhfmfatv4321@gmail.com>
2024-05-09 11:48:09 +0700

    08/05/2024

--- Other/0000_test.cpp
+++ Other/0000_test.cpp
@@ -1,28 +1,20 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main()
 {
-    int n, m;
-    cin >> n >> m;
-    int A[n][m];
-    int B[n];
-    // fill(A[0],A[0] + n*m,100);
-    fill(B,B+n,10);
-    fill_n(A[0],n*m,1);
-    for (int j = 0; j < n; j++)
+    ios_base::sync_with_stdio(0);
+    string s;
+    cin >> s;
+    int A[s.length()];
+    for (int i = 0; i < s.length(); i++)
     {
-        cout << B[j] << " ";
+        A[i] = s[i] - 'A' + 'A';
+        cout << s[i]-'A'+'A' << " ";
     }
     cout << "\n";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < s.length(); i++)
     {
-        for (int j = 0; j < m; j++)
-        {
-            cout << A[i][j] << " ";
-        }
-        cout << "\n";
+        cout << A[i]-65 << " ";
     }
     return 0;
 }
