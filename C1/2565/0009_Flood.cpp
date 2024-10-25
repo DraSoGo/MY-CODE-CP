e94854556ef125d1bc3f65b031612f84e6d6e0aa
DraSoGo <vdhfmfatv4321@gmail.com>
2024-10-25 13:45:11 +0700

    25/10/2024

--- C1/2565/0009_Flood.cpp
+++ C1/2565/0009_Flood.cpp
@@ -1,17 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+const int sz = 1e6+1;
+int A[sz];
+
 int main()
 {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int n;
     cin >> n;
-    int N[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> N[i];
+        cin >> A[i];
     }
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n-1; i++)
     {
-        /* code */
+        if (A[i] < A[i+1])
+        {
+            cout << A[i] << " ";
+        }
+        else
+        {
+            int idx = i+1,idxmx = i+1;
+            while (idx < n)
+            {
+                if (A[idx] > A[idxmx])
+                {
+                    idxmx = idx;
+                }
+                if (A[idxmx] > A[i])
+                {
+                    break;
+                }
+                idx++;
+            }
+            for (int j = i; j < idxmx; j++)
+            {
+                cout << min(A[i],A[idxmx]) << " ";
+            }
+            i = idxmx-1;
+        }
+        
     }
-    
+    return 0;
 }
